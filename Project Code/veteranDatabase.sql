2d8b2ebdcc367372be1d82ba40acc3f60c6bf2a9
guffyj <guffy.jenna@gmail.com>
2025-01-20 15:24:25 -0500

    Fixed all errors in SQL code- now imports in phpMyAdmin without errors. Updated ERD Draft to match- JG

--- Project Code/veteranDatabase.sql
+++ Project Code/veteranDatabase.sql
@@ -5,8 +5,8 @@ CREATE TABLE student(
     stu_street VARCHAR(30),
     stu_city VARCHAR(30),
     stu_state VARCHAR(2),
-    stu_zip INTEGER,
-    stu_area_code INTEGER,
+    stu_zip INT(5),
+    stu_area_code INT(3),
     stu_phone VARCHAR (12), /*12 chars to account for . or - between area code and exchange*/
     stu_email VARCHAR (40),
     stu_aid_bal_months INTEGER,
@@ -19,7 +19,7 @@ CREATE TABLE certification(
     stu_id VARCHAR(15),
     cert_date DATE,
     cert_status TINYINT(1), /*FOR BOOLEAN VALUE- 0 IS FALSE 1 IS TRUE*/
-    FOREIGN KEY(stu_id) REFERENCES student(stu_id) ON DELETE CASCADE
+    FOREIGN KEY (stu_id) REFERENCES student(stu_id) ON DELETE CASCADE
 );
 
 CREATE TABLE staff(
@@ -36,19 +36,19 @@ CREATE TABLE report(
     report_num INT AUTO_INCREMENT PRIMARY KEY,
     staff_id VARCHAR(15),
     report_date DATE, 
-    report_type VARCHAR (15)
+    report_type VARCHAR(15),
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id) ON DELETE CASCADE
 );
 
 CREATE TABLE email_template(
     tmplt_id INT AUTO_INCREMENT PRIMARY KEY,
     tmplt_subject VARCHAR(100),
-    tmplt_body LONGTEXT,
+    tmplt_body LONGTEXT
 );
 
 CREATE TABLE scheduled_email(
     sch_email_id INT AUTO_INCREMENT PRIMARY KEY,
-    tmplt_id INTEGER,
+    tmplt_id INT,
     FOREIGN KEY (tmplt_id) REFERENCES email_template(tmplt_id)
 );
 
@@ -56,11 +56,10 @@ CREATE TABLE email_to_student(
     email_id INT AUTO_INCREMENT PRIMARY KEY,
     stu_id VARCHAR(15),
     staff_id VARCHAR(15),
-    tmplt_id INTEGER, 
-    stu_id VARCHAR(15),
-    sch_email_id INTEGER,
+    tmplt_id INT, 
+    sch_email_id INT,
     FOREIGN KEY (stu_id) REFERENCES student(stu_id) ON DELETE CASCADE, 
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id) ON DELETE CASCADE, 
     FOREIGN KEY (tmplt_id) REFERENCES email_template(tmplt_id) ON DELETE CASCADE,
-    FOREIGN KEY (sch_email_id) REFERENCES sch_email_id(scheduled_email) ON DELETE CASCADE
-)
+    FOREIGN KEY (sch_email_id) REFERENCES scheduled_email(sch_email_id) ON DELETE CASCADE
+);
