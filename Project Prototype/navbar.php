a7c80146e8d9689b379ac9c3c69e6b4da8eb25a1
Andrew Evans <eva54587@pennwest.edu>
2025-02-19 23:15:26 -0500

    CSS and HTML changes
    Cleaned up and improved the styles.css file.
    Added a navbar.php file
    Inside inedx.html I added a commented out implementation of the navbar for when all the HTML files are converted to php in the next sprint

---
+++ Project Prototype/navbar.php
@@ -0,0 +1,14 @@
+
+
+<nav>
+    <a href="index.html">Home</a>
+    <a href="search.html">Search</a>
+    <a href="newrecord.html">New Record</a>
+    <a href="email.html">Email</a>
+    <a href="reports.html">Reports</a>
+    &ensp;&ensp;&ensp;&ensp;<a href="login.html">Login</a>
+    <a href="#logout">| Logout</a>
+</nav>
+
+
+
