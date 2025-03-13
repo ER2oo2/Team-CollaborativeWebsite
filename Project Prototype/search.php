9d31d9c394b3ee419de07961136e515041a791af
guffyj <guffy.jenna@gmail.com>
2025-03-13 11:39:05 -0400

    Updated stylesheet for search, added folder for Sprint 4 deliverables and included all files needed for submission

--- Project Prototype/search.php
+++ Project Prototype/search.php
@@ -78,7 +78,7 @@ if (isset($_POST['search-option'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search Students</title>
-    <link rel="stylesheet" href="../styles.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
